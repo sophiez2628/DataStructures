beea07166a9ffe538d2bd11845fbec7daf528eb2
Sophie Zhao <sophie.zhao@ucla.edu>
2015-11-20 00:52:58 -0800

    add dbs method

--- ruby/tree/00_tree_node/lib/00_tree_node.rb
+++ ruby/tree/00_tree_node/lib/00_tree_node.rb
@@ -1,3 +1,46 @@
-class PolyTreeNode
+module Searchable
+  def dfs(target_value)
+    if self.value == target_value
+      return self
+    else
+      self.children.each do |child|
+        return_value = child.dfs(target_value)
+        return return_value if return_value
+      end
+    end
+    return nil
+  end
 
+  def bfs(target_value)
+
+  end
+end
+
+class PolyTreeNode
+  include Searchable
+
+  attr_reader :parent
+  attr_accessor :children, :value
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(new_parent)
+    if parent
+      parent.children.delete(self)
+    end
+      @parent = new_parent
+      parent.children << self if new_parent
+  end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child)
+    raise "not a child" unless children.include?(child)
+    child.parent = nil
+  end
 end
