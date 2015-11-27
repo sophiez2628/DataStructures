e2992a636a27a5b9daf937139d527012ba629ed0
Sophie Zhao <sophie.zhao@ucla.edu>
2015-11-26 22:44:25 -0800

    complete heapify in place method

--- javascript/heap_sort.js
+++ javascript/heap_sort.js
@@ -3,9 +3,36 @@ Array.prototype.heapSort = function() {
     return this;
   } else {
     this.heapify();
-    this.unheapify();
+    // this.unheapify();
     return this;
   }
+}
+
+Array.prototype.heapify = function() {
+  //iterative solution
+  this.forEach(function(num, index){
+    //for each executes a provided function once per array element
+    var lastItem = index;
+    //maintain a max heap
+    if (index != 0) {
+      var childPos = index;
+      while (childPos > 0) {
+        var parentPos = this.parentPos(childPos);
+        if (this[childPos] > this[parentPos]) {
+          this.swap(childPos, parentPos);
+          childPos = parentPos;
+        } else {
+          break;
+        }
+      }
+    }
+  }.bind(this))
+}
+
+Array.prototype.swap = function(pos1, pos2) {
+  var temp = this[pos1];
+  this[pos1] = this[pos2];
+  this[pos2] = temp;
 }
 
 Array.prototype.parentPos = function(childPos) {
@@ -14,11 +41,11 @@ Array.prototype.parentPos = function(childPos) {
 
 Array.prototype.childrenPos = function(parentPos) {
   childrenPos = [];
-  if (parentPos * 2 + 1 <= this.arr.length - 1) {
+  if (parentPos * 2 + 1 <= this.length - 1) {
     childrenPos.push(parentPos * 2 + 1);
   }
 
-  if (parentPos * 2 + 2 <= this.arr.length - 1) {
+  if (parentPos * 2 + 2 <= this.length - 1) {
     childrenPos.push(parentPos * 2 + 2);
   }
   return childrenPos;
