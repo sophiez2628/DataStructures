b5861d0fb3eddfc7ed4f66207ace22613c12de0a
Sophie Zhao <sophie.zhao@ucla.edu>
2015-11-26 20:35:55 -0800

    refactor heapify method

--- javascript/heap.js
+++ javascript/heap.js
@@ -10,7 +10,15 @@ MinHeap.prototype.parentPos = function(childPos) {
 }
 
 MinHeap.prototype.childrenPos = function(parentPos) {
-  return [parentPos * 2 + 1, parentPos * 2 + 2];
+  childrenPos = [];
+  if (parentPos * 2 + 1 < this.arr.length - 1) {
+    childrenPos.push(parentPos * 2 + 1);
+  }
+
+  if (parentPos * 2 + 2 < this.arr.length - 1) {
+    childrenPos.push(parentPos * 2 + 2);
+  }
+  return childrenPos;
 }
 
 MinHeap.prototype.insert = function(val) {
@@ -37,4 +45,28 @@ MinHeap.prototype.heapifyUp = function(childPos) {
 
 MinHeap.prototype.min = function() {
   return this.arr[0];
+}
+
+MinHeap.prototype.heapifyDown = function(parentPos) {
+  childrenPos = this.childrenPos(parentPos);
+  if (childrenPos.length == 1) {
+    this.swap(childrenPos, parentPos);
+    this.heapifyDown(childrenPos);
+  } else {
+    return this.arr;
+  }
+}
+
+MinHeap.prototype.popMin = function() {
+  min = this.arr[0];
+  if (this.arr <= 1) {
+    return min;
+  } else {
+    tail = this.arr.pop();
+    this.arr[0] = tail;
+
+    if (this.arr.length > 1) {
+      this.heapifyDown(0);
+    }
+  }
 }
