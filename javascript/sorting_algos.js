af0ecb5d822423e8c756bfb790b0c4c12c5ba7de
Sophie Zhao <sophie.zhao@ucla.edu>
2015-11-28 16:58:51 -0800

    complete quicksort algorithm

--- javascript/sorting_algos.js
+++ javascript/sorting_algos.js
@@ -29,3 +29,42 @@ Array.prototype.mergeHelper = function(left, right) {
   sorted = sorted.concat(right);
   return sorted;
 }
+
+//how to find the median of a stream of integers? build min and max heap
+//store numbers greater than median to min heap, numbers smaller than median to max heap
+
+//quicksort
+Array.prototype.quickSort = function() {
+  var sorted = []
+  if (this.length < 2) {
+    return this;
+  } else {
+    this.swap(0, Math.floor(Math.random() * (this.length - 1)));
+    var pivotIdx = this.partition();
+    sorted = this.slice(0,pivotIdx).quickSort().concat([this[pivotIdx]]);
+    return sorted.concat(this.slice(pivotIdx + 1).quickSort());
+  }
+}
+
+Array.prototype.partition = function() {
+  var pivotIdx = 0;
+  for(var i = 1; i < this.length; i++) {
+    if (this[i] < this[pivotIdx]) {
+      if (i == pivotIdx + 1) {
+        this.swap(i, pivotIdx);
+        pivotIdx = i;
+      } else {
+        this.swap(i, pivotIdx + 1);
+        this.swap(pivotIdx, pivotIdx + 1);
+        pivotIdx = pivotIdx + 1;
+      }
+    }
+  }
+  return pivotIdx;
+}
+
+Array.prototype.swap = function(pos1, pos2) {
+  var temp = this[pos1];
+  this[pos1] = this[pos2];
+  this[pos2] = temp;
+}
