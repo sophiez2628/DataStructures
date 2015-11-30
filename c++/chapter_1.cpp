9705bb8bd967da89e831fac7bb4ae594de0aab72
Sophie Zhao <sophie.zhao@ucla.edu>
2015-11-30 11:28:25 -0800

    update c++ notes

---
+++ c++/chapter_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello world!" << std::endl;
+  return 0;
+}
+
+// std::endl creates a newline
+//functions cannot be defined inside other functions in c++ 
