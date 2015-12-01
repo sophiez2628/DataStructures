8c671f1d1a97e0484dde82c976e3c621ad52942f
Sophie Zhao <sophie.zhao@ucla.edu>
2015-11-30 19:12:28 -0800

    tail recursion in js

--- javascript/pair_boarding_problems/set_10.js
+++ javascript/pair_boarding_problems/set_10.js
@@ -22,7 +22,7 @@ function binaryForm(int) {
 // Implement factorial with and without recursion.
 // What is a potential disadvantage of the recursive way?
 
-// What is tail-recursion? Does JS have tail-call optimization?
+// What is tail-recursion? Does Ruby have tail-call optimization?
 // Pretend it did; write a tail-recursive version of rec_fac.
 
 function fac(n) {
