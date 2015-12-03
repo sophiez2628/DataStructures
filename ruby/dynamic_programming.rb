20015fa94c9b0b9df80e4c63a224e28980f7f609
Sophie Zhao <sophie.zhao@ucla.edu>
2015-12-03 00:43:57 -0800

    solve make change problem using dynamic programming

--- ruby/dynamic_programming.rb
+++ ruby/dynamic_programming.rb
@@ -23,3 +23,19 @@ def fib_tab(n)
   end
   cache.last
 end
+
+#coin change problem - takes in sorted array of coins
+
+def make_change(coins, sum, hash)
+  return false if coins.first > sum
+  hash[sum] = [coins.first] if coins.first == sum
+  make_change(coins, sum - 1, hash)
+  coins.each do |coin|
+    next if coin > sum
+    remainder = sum - coin
+    if hash[sum].length == 0 || hash[remainder].length + 1 < hash[sum].length
+      hash[sum] = hash[remainder].dup + [coin]
+    end
+  end
+  return hash[sum]
+end
