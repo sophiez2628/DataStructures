818cb8c542df379fd4f8985c5a18bd64e9d426cc
Sophie Zhao <sophie.zhao@ucla.edu>
2015-12-05 20:14:16 -0800

    complete tabs using jquery

--- javascript/jquery/tabs/tabs.js
+++ javascript/jquery/tabs/tabs.js
@@ -1,19 +1,40 @@
 //namespace
 $.Tabs = function (el) {
   //el passed in as a HTML element
+  //this.$el is the ul
   this.$el = $(el);
+  //section
   this.$contentTabs = $(this.$el.data("content-tabs"));
+  //article
   this.$activeTab = $(this.$contentTabs.find(".active"));
-
   this.$el.on('click','a',this.clickTab.bind(this));
 
 };
 
 $.Tabs.prototype.clickTab = function(e) {
+  //e.currentTarget is the anchor tag clicked on
+  e.preventDefault();
+
+  //remove active from current anchor
+  this.$el.find("a").removeClass("active");
+
+  //remove active from prev tab
+  this.$activeTab.toggleClass("active").addClass("transitioning");
+
+  //add active to new anchor tag
+  $(e.currentTarget).toggleClass("active");
+
+  this.$activeTab.one("transitionend", function() {
+    this.$activeTab.removeClass("transitioning");
+    //add active to new tab, find takes a selector!
+    var tabName = $(e.currentTarget).attr("href");
+    this.$activeTab = this.$contentTabs.find(tabName).toggleClass("active");
+
+  }.bind(this))
 
 };
 
-//extends jquery functions 
+//extends jquery functions
 $.fn.tabs = function () {
   return this.each(function () {
     new $.Tabs(this);
