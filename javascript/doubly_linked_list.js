908e5c3b0dceff09ad80f917de3b7e08326a09fb
Sophie Zhao <sophie.zhao@ucla.edu>
2015-12-13 19:28:36 -0800

    bfs using implemented queue

--- javascript/doubly_linked_list.js
+++ javascript/doubly_linked_list.js
@@ -31,3 +31,60 @@ DoublyLinkedList.prototype.insert = function(val) {
 
   return newLink;
 }
+
+DoublyLinkedList.prototype.removeFirst = function() {
+  var removed = this.head.nxt;
+  this.head.nxt = removed.nxt;
+  removed.nxt.prev = this.head;
+
+  return removed;
+}
+
+//build queue using doubly linked list
+var Queue = function() {
+  this.store = new DoublyLinkedList();
+}
+
+Queue.prototype.enqueue = function(val) {
+  this.store.insert(val);
+}
+
+Queue.prototype.dequeue = function() {
+  return this.store.removeFirst();
+}
+
+Queue.prototype.empty = function() {
+  return this.store.empty();
+}
+
+//implement bfs using queue, so that runtime is linear
+//graph given is an adjacency matrix
+
+//{} is not the same as a hash table in javascript
+var graph = [
+  [0,1,0,1],
+  [0,0,1,0],
+  [1,0,0,1],
+  [0,0,0,0]
+];
+
+var bfs = function(graph, src, target) {
+  var visited = {};
+  var q = new Queue();
+  q.enqueue(src);
+  while (q.empty() === false) {
+    var node = q.dequeue();
+    
+    if (node.val === target) {
+      return node.val;
+    };
+
+    for(var i = 0; i < graph[src].length; i++) {
+      if (!visited[src] && graph[src][i] === 1) {
+        q.enqueue(i);
+      }
+    }
+    visited[src] = true;
+  }
+  return "Target not found";
+}
